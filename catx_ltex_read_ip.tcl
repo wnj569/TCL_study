ae5a09161d37d832543da83542ade84558ce8308
Yikang Zhang <106954726+Yikangz1@users.noreply.github.com>
2022-08-02 13:56:23 +0800

    read IP

---
+++ catx_ltex_read_ip.tcl
@@ -0,0 +1,13 @@
+#!/usr/bin/tclsh
+
+#read vivado ooc ip
+#read_ip ../ip/znanop_exsys_clkin.xci
+
+read_ip	../ip/ltec_mem/blk_mem_gen_1024X64M8/blk_mem_gen_1024X64M8.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_10816X64M8/blk_mem_gen_10816X64M8.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_352X64M4/blk_mem_gen_352X64M4.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_384X128M8/blk_mem_gen_384X128M8.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_384X64M8/blk_mem_gen_384X64M8.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_64X96M1/blk_mem_gen_64X96M1.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_8192X64M8/blk_mem_gen_8192X64M8.xci
+read_ip	../ip/ltec_mem/blk_mem_gen_96x32M1/blk_mem_gen_96x32M1.xci
